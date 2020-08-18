9b4293eaf997c818d33d84d446dab53c8aead908
zemo37 <zelenkin02@ukr.net>
2020-08-18 14:50:51 +0300

    Create

---
+++ EvenOdd/EvenOdd/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = Convert.ToInt32(Console.ReadLine());
+            int end = Convert.ToInt32(Console.ReadLine());
+            int counteven = 0;
+            int countodd = 0;
+            while (start<=end)
+            {
+                Console.WriteLine(start);
+                if (start % 2 == 0) counteven++;
+                else countodd++;
+                 start++; 
+            }
+            Console.WriteLine($"Even: {counteven} \nodd: {countodd}");
+        }
+    }
+}
